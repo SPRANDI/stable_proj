2c74a43cc8e345aa5ab05dbc40d084fb1607f992
SPRANDI <daniildevolution12@gmail.com>
2026-02-26 20:35:59 +0300

    Update QuickEdit.py
    1.0.0

--- QuickEdit.py
+++ QuickEdit.py
@@ -54,17 +54,17 @@ while exit_flag:
             time.sleep(1.0)
 
         elif decide == 1:
-            name_file = input("Name file: ")
-            file = open(name_file, "w")
-            info = input("File contents: ")
-            file.write(info)
-            file.close()
-            con = input("\nThe file was created successfully\n\nPress Enter to continue\n\n")
+            try:
+                name_file = input("Name file: ")
+                with open(name_file, "x", encoding='utf-8') as file:
+                    info = input("File contents: ")
+                    file.write(info)
+                    con = input("\nThe file was created successfully\n\nPress Enter to continue\n\n")
 
-            if con == "":
-                continue
-            else:
-                continue
+            except FileExistsError:
+                print("A file with that name already exists")
+                con = input("\n\nPress Enter to continue\n\n")
+
 
         elif decide == 2:
             decide_edit = int(input(" 1. Overwrite file\n 2. Add to file\n >>> "))
@@ -73,22 +73,23 @@ while exit_flag:
                 time.sleep(1.0)
 
             elif decide_edit == 1:
+
                 decide_edit_file = input("Name file: ")
+
+                if not os.path.exists(decide_edit_file):
+                    print("The file was not found")
+                    con = input("\n\nPress Enter to continue\n\n")
+                    continue   
+
                 overwrite = input("Overwrite file: ")
                 dec = input("Overwrite file? (y/n): ")
+            
+                if dec == "y" or dec == "Y":
+                    with open(decide_edit_file, "w", encoding='utf-8') as file:
+                        file.write(overwrite)
+                        con = input("\nThe file was edited successfully\n\nPress Enter to continue\n\n")
 
-                if dec == "y":
-                    file = open(decide_edit_file, "w")
-                    file.write(overwrite)
-                    file.close()
-                    con = input("\nThe file was edited successfully\n\nPress Enter to continue\n\n")
-
-                    if con == "":
-                        continue
-                    else:
-                        continue
-
-                elif dec == "n":
+                elif dec == "n" or dec == "N":
                     continue
 
             elif decide_edit == 2:
@@ -96,57 +97,46 @@ while exit_flag:
                 append = input("Add file: ")
                 dec = input("Add file? (y/n): ")
 
-                if dec == "y":
-                    file = open(decide_edit_file, "a")
+                if dec == "y" or dec == "Y":
+                    file = open(decide_edit_file, "a", encoding='utf-8')
                     file.write(f"\n{append}")
                     file.close()
                     con = input("\nThe file was edited successfully\n\nPress Enter to continue\n\n")
 
-                    if con == "":
-                        continue
-                    else:
-                        continue
-
-                elif dec == "n":
+                elif dec == "n" or dec == "N":
                     continue
 
         elif decide == 3:
-            decide_edit_file = input("Name file: ")
-            file = open(decide_edit_file, "r")
-            print(file.read())
-            file.close()
-            con = input("\n\nPress Enter to continue\n\n")
+            try:
+                decide_edit_file = input("Name file: ")
+                with open(decide_edit_file, "r", encoding='utf-8') as file:
+                    print(file.read())
+                    con = input("\n\nPress Enter to continue\n\n")
 
-            if con == "":
-                continue
-            else:
-                continue
+            except FileNotFoundError:
+                print("The file was not found ")
+                con = input("\n\nPress Enter to continue\n\n")
 
         elif decide == 4:
-            decide_del_file = input("Name file: ")
-            decc = input("Delete file? (y/n): ")
-            if decc == "y":
-                os.remove(decide_del_file)
-                print("The file was deleted successfully")
-                con = input("\n\nPress Enter to continue\n\n")
+            try:
+                decide_del_file = input("Name file: ")
+                decc = input("Delete file? (y/n): ")
+                if decc == "y" or decc == "Y":
+                    os.remove(decide_del_file)
+                    print("\nThe file was deleted successfully")
+                    con = input("\n\nPress Enter to continue\n\n")
 
-                if con == "":
-                    continue
-                else:
-                    continue
-            else:
-                print("The file was not deleted")
+                elif decc == "n" or decc == "N":
+                    print("The file was not deleted")
+                    con = input("\nPress Enter to continue\n\n")
+            
+            except FileNotFoundError:
+                print("The file was not found ")
                 con = input("\n\nPress Enter to continue\n\n")
-
-                if con == "":
-                    continue
-                else:
-                    continue
 
         elif decide == 5:
             exit_flag = False
-
+        
     except ValueError:
-        time.sleep(0.5)
-        print(">>> Error pls enter a number <<<")
-        time.sleep(1.7)
+        print("Enter a number ")
+        con = input("\n\nPress Enter to continue\n\n")
